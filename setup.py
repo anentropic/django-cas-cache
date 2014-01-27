6300ac54f61b48438f58036776d1b6976b272063
Paul Garner <ego@anentropic.com>
2014-01-27 16:47:51 +0000

    pypi version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-cas-cache',
-    version='0.1.2',
+    version='0.1.3',
     packages=[
         'cascache',
         'cascache.backends',
