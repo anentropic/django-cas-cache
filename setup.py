bfc1d73f2933a30f28266080429fe54f2f3a4794
Paul Garner <ego@anentropic.com>
2014-01-27 16:55:19 +0000

    fix broken manifest

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-cas-cache',
-    version='0.1.3',
+    version='0.1.4',
     packages=[
         'cascache',
         'cascache.backends',
